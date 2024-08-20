460c0f89041d2eb4f95706ada3dbc0927bce8a46
nihicak <nihciak@hotmail.com>
2024-08-20 23:05:29 +0800

    Feat: Include previous incentive percentage change

--- miner_status.py
+++ miner_status.py
@@ -1,11 +1,20 @@
-import pexpect
+import os
 import re
+import json
+import pexpect
+import pathlib
 import requests
 
 SLACK_WEBHOOK_URL = "<slack webhook url>"
+
+FILE_DIR = pathlib.Path(__file__).parent.resolve()
+DATA_DIR = os.path.join(FILE_DIR, "incentive-data")
+
 LOCAL_NODE = False
+SHOW_CHANGE_PERCENTAGE = True
 SUBNET_IDS = []
 
+# alert report to slack webhook
 def alert_slack(message="-", title="", fields=[], color="#777777"):
     webhook = SLACK_WEBHOOK_URL
     payload = {
@@ -19,6 +28,71 @@ def alert_slack(message="-", title="", fields=[], color="#777777"):
     }
     req = requests.post(webhook, json=payload)
 
+# generate incentive report
+def init_data_directory(data_dir=DATA_DIR):
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+def get_subnet_from_title(title):
+    return title.split(' | ')[0].split(' ')[-1]
+
+def get_data_from_field(field):
+    title = field.get('title')
+    value = field.get('value')
+
+    if not value:
+        return title, None
+
+    return title, float(value.split(' | ')[-2].split(' ')[-1])
+
+def get_percentage_change(new_value, old_value):
+    return round(((new_value / old_value) - 1) * 100, 2)
+
+def get_incentive_change(sn_id, fields):
+    init_data_directory()
+    data_file = os.path.join(DATA_DIR, f"{sn_id}.json")
+
+    incentive_data = {}
+    if os.path.exists(data_file):
+        try:
+            with open(data_file) as f:
+                json_data = json.load(f)
+
+            prev_incentive_data = {}
+            for data in json_data:
+                title, last_incentive = get_data_from_field(data)
+                prev_incentive_data[title] = last_incentive
+
+            for data in fields:
+                title, incentive = get_data_from_field(data)
+                last_incentive = prev_incentive_data.get(title)
+
+                if last_incentive == None or incentive == None:
+                    incentive_data[title] = 0
+
+                incentive_data[title] = get_percentage_change(incentive, last_incentive)
+
+        except Exception as e:
+            print(e)
+
+    # rewrite with current data
+    with open(data_file, "w") as f:
+        f.write(json.dumps(fields))
+
+    return incentive_data
+
+def add_incentive_change(title, fields):
+    _fields = fields.copy()
+    change_data = get_incentive_change(get_subnet_from_title(title), _fields)
+    for i, field in enumerate(_fields):
+        change = change_data.get(field['title'])
+        if change:
+            change = f'+{change}' if change > 0 else change
+            _fields[i]['value'] = f"{_fields[i]['value']} `{change}%`"
+
+    return _fields
+
+# main function
 def report_status():
     fields = []
     title = ""
@@ -59,10 +133,21 @@ def report_status():
             elif len(items) < 15 and fields:
                 title = f"{title} | Stake: τ{stake} | Miners: {len(fields)}"
 
-                if not SUBNET_IDS:
-                    # send all
+                if not SUBNET_IDS: 
+                    # send all subnets
+                    # sort fields
+                    fields = sorted(fields, key=lambda d: d['title'])
+                    if SHOW_CHANGE_PERCENTAGE:
+                        fields = add_incentive_change(title, fields)
+
                     alert_slack('Miner Report', title, fields)
+
                 elif any(f"Subnet: {sn_id}" in title for sn_id in SUBNET_IDS):
+                    # sort fields
+                    fields = sorted(fields, key=lambda d: d['title'])
+                    if SHOW_CHANGE_PERCENTAGE:
+                        fields = add_incentive_change(title, fields)
+
                     alert_slack('Miner Report', title, fields)
                 
                 fields = []
@@ -72,7 +157,7 @@ def report_status():
                 continue
             
             fields.append(
-                { "title":f"uid {items[2]} [{items[1]}]", "value":f"Stake: τ{items[4]} | Trust: {items[6]} | Incentive: {items[8]}" }
+                { "title":f"{items[1]} [uid {items[2]}]", "value":f"St: τ{items[4]} | Tr: {items[6]} | In: {items[8]} | Em: {items[10]}" }
             )
 
             stake = stake + float(items[4])
